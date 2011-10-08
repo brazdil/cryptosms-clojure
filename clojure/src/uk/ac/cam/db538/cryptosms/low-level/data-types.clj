494aa523e7c40d230b841fffc14d74e9f2b83721
David Brázdil <david.brazdil@gmail.com>
2011-10-08 23:22:38 +0100

    Aligned exportable

--- clojure/src/uk/ac/cam/db538/cryptosms/low-level/data-types.clj
+++ clojure/src/uk/ac/cam/db538/cryptosms/low-level/data-types.clj
@@ -2,9 +2,8 @@
   (:use clojure.test))
 
 (defrecord ExportableType [export import length])
-(defrecord ExportableItem [name type])
 
-; NUMBERS
+; UNSIGNED INTEGERS
 
 (with-test
   (defn- 
@@ -150,3 +149,31 @@
       (is (= ((:export (composite items)) data) result))
       (is (= ((:import (composite items)) result) data))
       (is (= (:length (composite items)) (count result))) ))
+
+; ALIGN
+
+(with-test
+  (defn align [^Number length-aligned exportable]
+    (let [ length-random (- length-aligned (:length exportable)) ]
+      (if (< length-random 0) ; handles negative alignment length as well
+        (throw (new IllegalArgumentException))
+        (ExportableType.
+          ; export
+          (fn [data] (reduce conj ((:export exportable) data) (vec (repeatedly length-random #(rand-int 256)))))
+          ; import
+          (fn [^bytes xs]
+            (if (not= (count xs) length-aligned)
+              (throw (new IllegalArgumentException))
+              ((:import exportable) (subvec xs 0 (:length exportable)))))
+          ; length
+          length-aligned ))))
+  (let [ item (uint64 :id)
+         data { :id 0x1234567890ABCDEF }
+         result [ 0x12 0x34 0x56 0x78 0x90 0xAB 0xCD 0xEF ]
+         result-aligned [ 0x12 0x34 0x56 0x78 0x90 0xAB 0xCD 0xEF 0xFE 0xDC 0xBA 0x09 0x87 0x65 0x43 0x21 ] ]
+    (is (thrown? IllegalArgumentException (align -1 item)))
+    (is (thrown? IllegalArgumentException (align 7 item)))
+    (is (= (count ((:export (align 128 item)) data)) 128))
+    (is (= (subvec ((:export (align 128 item)) data) 0 8 ) result))
+    (is (= ((:import (align 16 item)) result-aligned) data))
+    (is (= (:length (align 128 item)) 128)) ))
