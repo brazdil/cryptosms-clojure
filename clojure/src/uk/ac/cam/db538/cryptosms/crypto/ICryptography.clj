c222edd5b13e1e1224de9fe406e2c6e7ac4f240d
David Brázdil <david.brazdil@gmail.com>
2011-10-09 14:33:54 +0100

    Removed unnecessary files

--- clojure/src/uk/ac/cam/db538/cryptosms/crypto/ICryptography.clj
+++
@@ -1,8 +0,0 @@
-(ns uk.ac.cam.db538.cryptosms.crypto.ICryptography
-  (:require [uk.ac.cam.db538.cryptosms.utils :as utils]))
-
-(gen-interface
-  :name       uk.ac.cam.db538.cryptosms.crypto.ICryptography
-  :methods    [
-                [encryptAES_CBC [#=(utils/type-byte-array), uk.ac.cam.db538.cryptosms.crypto.SymmetricKey] #=(utils/type-byte-array)]
-              ])
