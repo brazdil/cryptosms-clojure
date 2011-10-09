497b9a74a59d29e12a8830e479459687983baaec
David Brázdil <david.brazdil@gmail.com>
2011-10-09 14:05:47 +0100

    3rd party stuff project

--- android/src/uk/ac/cam/db538/cryptosms/crypto/BouncyCastle.java
+++ android/src/uk/ac/cam/db538/cryptosms/crypto/BouncyCastle.java
@@ -4,11 +4,11 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 
-import org.spongycastle.crypto.BlockCipher;
-import org.spongycastle.crypto.engines.AESFastEngine;
-import org.spongycastle.crypto.modes.CBCBlockCipher;
-import org.spongycastle.crypto.params.KeyParameter;
-import org.spongycastle.crypto.params.ParametersWithIV;
+//import org.spongycastle.crypto.BlockCipher;
+//import org.spongycastle.crypto.engines.AESFastEngine;
+//import org.spongycastle.crypto.modes.CBCBlockCipher;
+//import org.spongycastle.crypto.params.KeyParameter;
+//import org.spongycastle.crypto.params.ParametersWithIV;
 
 import uk.ac.cam.db538.cryptosms.utils.Numeric;
 
@@ -16,41 +16,42 @@ public class BouncyCastle implements ICryptography {
 	
 	private static SecureRandom sRandom = null;
 	
-	private SecureRandom getRandom() {
-		if (sRandom == null)
-			try {
-				sRandom = SecureRandom.getInstance("SHA1PRNG", "SUN");
-			} catch (NoSuchAlgorithmException e) {
-				throw new RuntimeException(e);
-			} catch (NoSuchProviderException e) {
-				throw new RuntimeException(e);
-			}
-		return sRandom;
-	}
-	
-	private byte[] getRandomBytes(int length) {
-		byte[] random = new byte[length];
-		getRandom().nextBytes(random);
-		return random;
-	}
-	
-	private byte[] getOutcome(BlockCipher cipher, byte[] data) {
-		int lengthExpected = Numeric.leastGreaterMultiple(data.length, cipher.getBlockSize());
-		byte[] result = new byte[lengthExpected];
-		int lengthProcessed = cipher.processBlock(data, 0, result, 0);
-		
-		if (lengthExpected != lengthProcessed)
-			throw new RuntimeException("Block cipher produced unexpected data");
-		
-		return result;
-	}
+//	private SecureRandom getRandom() {
+//		if (sRandom == null)
+//			try {
+//				sRandom = SecureRandom.getInstance("SHA1PRNG", "SUN");
+//			} catch (NoSuchAlgorithmException e) {
+//				throw new RuntimeException(e);
+//			} catch (NoSuchProviderException e) {
+//				throw new RuntimeException(e);
+//			}
+//		return sRandom;
+//	}
+//	
+//	private byte[] getRandomBytes(int length) {
+//		byte[] random = new byte[length];
+//		getRandom().nextBytes(random);
+//		return random;
+//	}
+//	
+//	private byte[] getOutcome(BlockCipher cipher, byte[] data) {
+//		int lengthExpected = Numeric.leastGreaterMultiple(data.length, cipher.getBlockSize());
+//		byte[] result = new byte[lengthExpected];
+//		int lengthProcessed = cipher.processBlock(data, 0, result, 0);
+//		
+//		if (lengthExpected != lengthProcessed)
+//			throw new RuntimeException("Block cipher produced unexpected data");
+//		
+//		return result;
+//	}
 
 	@Override
 	public byte[] encryptAES_CBC(byte[] data, SymmetricKey key) {
-		AESFastEngine aes = new AESFastEngine();
-		CBCBlockCipher cbc = new CBCBlockCipher(aes);
-		
-		cbc.init(true, new ParametersWithIV(new KeyParameter(key.getBytes()), getRandomBytes(cbc.getBlockSize())));
-		return getOutcome(cbc, data);
+//		AESFastEngine aes = new AESFastEngine();
+//		CBCBlockCipher cbc = new CBCBlockCipher(aes);
+//		
+//		cbc.init(true, new ParametersWithIV(new KeyParameter(key.getBytes()), getRandomBytes(cbc.getBlockSize())));
+//		return getOutcome(cbc, data);
+		return null;
 	}
 }
