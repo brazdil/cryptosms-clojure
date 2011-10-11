138dbeacad2ccec82eca0a441d1980aaaf2463e6
David Brázdil <david.brazdil@gmail.com>
2011-10-11 23:45:55 +0100

    AES decrypt

--- core/src/uk/ac/cam/db538/cryptosms/crypto/aes.clj
+++ core/src/uk/ac/cam/db538/cryptosms/crypto/aes.clj
@@ -10,15 +10,30 @@
            (org.spongycastle.crypto.params KeyParameter)
            (org.spongycastle.crypto.params ParametersWithIV)))
 
+; encrypt/decrypt tested both together below
+
+(defn encrypt-aes-cbc [ data crypto-key crypto-iv ]
+  (let [ cipher (new CBCBlockCipher (new AESFastEngine))
+         iv-bytes (if (nil? crypto-iv) (random/rand-next-bytes (. cipher getBlockSize)) (byte-arrays/output crypto-iv)) ]
+    (. cipher init true 
+      (new ParametersWithIV 
+        (new KeyParameter (byte-arrays/output crypto-key)) 
+        iv-bytes))
+    (block-cipher/outcome cipher data)))
+
+(defn decrypt-aes-cbc [ data crypto-key crypto-iv ]
+  (let [ cipher (new CBCBlockCipher (new AESFastEngine))
+         iv-bytes (byte-arrays/output crypto-iv) ]
+    (. cipher init false 
+      (new ParametersWithIV 
+        (new KeyParameter (byte-arrays/output crypto-key)) 
+        iv-bytes))
+    (block-cipher/outcome cipher data)))
+
 (with-test
-  (defn encrypt-aes-cbc [ data crypto-key crypto-iv ]
-	  (let [ cipher (new CBCBlockCipher (new AESFastEngine))
-	         iv-bytes (if (nil? crypto-iv) (random/rand-next-bytes (. cipher getBlockSize)) (byte-arrays/output crypto-iv)) ]
-	    (. cipher init true 
-	      (new ParametersWithIV 
-	        (new KeyParameter (byte-arrays/output crypto-key)) 
-	        iv-bytes))
-	    (block-cipher/outcome cipher data)))
+  (defn- test-aes-cbc [ data crypto-key crypto-iv result ]
+    (is (= (encrypt-aes-cbc data crypto-key crypto-iv) result))
+    (is (= (decrypt-aes-cbc result crypto-key crypto-iv) data)))
   (let [ 
          key-128   (HEX "2b7e151628aed2a6abf7158809cf4f3c")
          key-192   (HEX "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b")
@@ -39,65 +54,63 @@
          iv10      (HEX "39F23369A9D9BACFA530E26304231461")
        ]
     ; 128-bit, 1 block
-    (is (= (encrypt-aes-cbc data1 key-128 iv01) (HEX "7649abac8119b246cee98e9b12e9197d")))
-    (is (= (encrypt-aes-cbc data2 key-128 iv02) (HEX "5086cb9b507219ee95db113a917678b2")))
-    (is (= (encrypt-aes-cbc data3 key-128 iv03) (HEX "73bed6b8e3c1743b7116e69e22229516")))
-    (is (= (encrypt-aes-cbc data4 key-128 iv04) (HEX "3ff1caa1681fac09120eca307586e1a7")))
+    (test-aes-cbc data1 key-128 iv01 (HEX "7649abac8119b246cee98e9b12e9197d"))
+    (test-aes-cbc data2 key-128 iv02 (HEX "5086cb9b507219ee95db113a917678b2"))
+    (test-aes-cbc data3 key-128 iv03 (HEX "73bed6b8e3c1743b7116e69e22229516"))
+    (test-aes-cbc data4 key-128 iv04 (HEX "3ff1caa1681fac09120eca307586e1a7"))
     ; 192-bit, 1 block
-    (is (= (encrypt-aes-cbc data1 key-192 iv01) (HEX "4f021db243bc633d7178183a9fa071e8")))
-    (is (= (encrypt-aes-cbc data2 key-192 iv05) (HEX "b4d9ada9ad7dedf4e5e738763f69145a")))
-    (is (= (encrypt-aes-cbc data3 key-192 iv06) (HEX "571b242012fb7ae07fa9baac3df102e0")))
-    (is (= (encrypt-aes-cbc data4 key-192 iv07) (HEX "08b0e27988598881d920a9e64f5615cd")))
+    (test-aes-cbc data1 key-192 iv01 (HEX "4f021db243bc633d7178183a9fa071e8"))
+    (test-aes-cbc data2 key-192 iv05 (HEX "b4d9ada9ad7dedf4e5e738763f69145a"))
+    (test-aes-cbc data3 key-192 iv06 (HEX "571b242012fb7ae07fa9baac3df102e0"))
+    (test-aes-cbc data4 key-192 iv07 (HEX "08b0e27988598881d920a9e64f5615cd"))
     ; 256-bit, 1 block
-    (is (= (encrypt-aes-cbc data1 key-256 iv01) (HEX "f58c4c04d6e5f1ba779eabfb5f7bfbd6")))
-    (is (= (encrypt-aes-cbc data2 key-256 iv08) (HEX "9cfc4e967edb808d679f777bc6702c7d")))
-    (is (= (encrypt-aes-cbc data3 key-256 iv09) (HEX "39f23369a9d9bacfa530e26304231461")))
-    (is (= (encrypt-aes-cbc data4 key-256 iv10) (HEX "b2eb05e2c39be9fcda6c19078c6a9d1b")))
-    )
+    (test-aes-cbc data1 key-256 iv01 (HEX "f58c4c04d6e5f1ba779eabfb5f7bfbd6"))
+    (test-aes-cbc data2 key-256 iv08 (HEX "9cfc4e967edb808d679f777bc6702c7d"))
+    (test-aes-cbc data3 key-256 iv09 (HEX "39f23369a9d9bacfa530e26304231461"))
+    (test-aes-cbc data4 key-256 iv10 (HEX "b2eb05e2c39be9fcda6c19078c6a9d1b")) )
   ; 128-bit, 1 block, changing data
   (let [
          key-128 (HEX "00000000000000000000000000000000") 
          iv      (HEX "00000000000000000000000000000000")
        ]
-    (is (= (encrypt-aes-cbc (HEX "f34481ec3cc627bacd5dc3fb08f273e6") key-128 iv) (HEX "0336763e966d92595a567cc9ce537f5e")))
-    (is (= (encrypt-aes-cbc (HEX "9798c4640bad75c7c3227db910174e72") key-128 iv) (HEX "a9a1631bf4996954ebc093957b234589")))
-    (is (= (encrypt-aes-cbc (HEX "96ab5c2ff612d9dfaae8c31f30c42168") key-128 iv) (HEX "ff4f8391a6a40ca5b25d23bedd44a597")))
-    (is (= (encrypt-aes-cbc (HEX "6a118a874519e64e9963798a503f1d35") key-128 iv) (HEX "dc43be40be0e53712f7e2bf5ca707209")))
-    (is (= (encrypt-aes-cbc (HEX "cb9fceec81286ca3e989bd979b0cb284") key-128 iv) (HEX "92beedab1895a94faa69b632e5cc47ce")))
-    (is (= (encrypt-aes-cbc (HEX "b26aeb1874e47ca8358ff22378f09144") key-128 iv) (HEX "459264f4798f6a78bacb89c15ed3d601")))
-    (is (= (encrypt-aes-cbc (HEX "58c8e00b2631686d54eab84b91f0aca1") key-128 iv) (HEX "08a4e2efec8a8e3312ca7460b9040bbf"))))
+    (test-aes-cbc (HEX "f34481ec3cc627bacd5dc3fb08f273e6") key-128 iv (HEX "0336763e966d92595a567cc9ce537f5e"))
+    (test-aes-cbc (HEX "9798c4640bad75c7c3227db910174e72") key-128 iv (HEX "a9a1631bf4996954ebc093957b234589"))
+    (test-aes-cbc (HEX "96ab5c2ff612d9dfaae8c31f30c42168") key-128 iv (HEX "ff4f8391a6a40ca5b25d23bedd44a597"))
+    (test-aes-cbc (HEX "6a118a874519e64e9963798a503f1d35") key-128 iv (HEX "dc43be40be0e53712f7e2bf5ca707209"))
+    (test-aes-cbc (HEX "cb9fceec81286ca3e989bd979b0cb284") key-128 iv (HEX "92beedab1895a94faa69b632e5cc47ce"))
+    (test-aes-cbc (HEX "b26aeb1874e47ca8358ff22378f09144") key-128 iv (HEX "459264f4798f6a78bacb89c15ed3d601"))
+    (test-aes-cbc (HEX "58c8e00b2631686d54eab84b91f0aca1") key-128 iv (HEX "08a4e2efec8a8e3312ca7460b9040bbf")))
   ; 256-bit, 1 block, changing data
   (let [
          key-256 (HEX "0000000000000000000000000000000000000000000000000000000000000000") 
          iv      (HEX "00000000000000000000000000000000")
        ]
-    (is (= (encrypt-aes-cbc (HEX "014730f80ac625fe84f026c60bfd547d") key-256 iv) (HEX "5c9d844ed46f9885085e5d6a4f94c7d7")))
-    (is (= (encrypt-aes-cbc (HEX "0b24af36193ce4665f2825d7b4749c98") key-256 iv) (HEX "a9ff75bd7cf6613d3731c77c3b6d0c04")))
-    (is (= (encrypt-aes-cbc (HEX "761c1fe41a18acf20d241650611d90f1") key-256 iv) (HEX "623a52fcea5d443e48d9181ab32c7421")))
-    (is (= (encrypt-aes-cbc (HEX "8a560769d605868ad80d819bdba03771") key-256 iv) (HEX "38f2c7ae10612415d27ca190d27da8b4")))
-    (is (= (encrypt-aes-cbc (HEX "91fbef2d15a97816060bee1feaa49afe") key-256 iv) (HEX "1bc704f1bce135ceb810341b216d7abe")))
- )
+    (test-aes-cbc (HEX "014730f80ac625fe84f026c60bfd547d") key-256 iv (HEX "5c9d844ed46f9885085e5d6a4f94c7d7"))
+    (test-aes-cbc (HEX "0b24af36193ce4665f2825d7b4749c98") key-256 iv (HEX "a9ff75bd7cf6613d3731c77c3b6d0c04"))
+    (test-aes-cbc (HEX "761c1fe41a18acf20d241650611d90f1") key-256 iv (HEX "623a52fcea5d443e48d9181ab32c7421"))
+    (test-aes-cbc (HEX "8a560769d605868ad80d819bdba03771") key-256 iv (HEX "38f2c7ae10612415d27ca190d27da8b4"))
+    (test-aes-cbc (HEX "91fbef2d15a97816060bee1feaa49afe") key-256 iv (HEX "1bc704f1bce135ceb810341b216d7abe")) )
  ; 256-bit, 1 block, changing key
  (let [
         data    (HEX "00000000000000000000000000000000") 
         iv      (HEX "00000000000000000000000000000000")
       ]
-   (is (= (encrypt-aes-cbc data (HEX "c47b0294dbbbee0fec4757f22ffeee3587ca4730c3d33b691df38bab076bc558") iv) (HEX "46f2fb342d6f0ab477476fc501242c5f")))
-   (is (= (encrypt-aes-cbc data (HEX "28d46cffa158533194214a91e712fc2b45b518076675affd910edeca5f41ac64") iv) (HEX "4bf3b0a69aeb6657794f2901b1440ad4")))
-   (is (= (encrypt-aes-cbc data (HEX "c1cc358b449909a19436cfbb3f852ef8bcb5ed12ac7058325f56e6099aab1a1c") iv) (HEX "352065272169abf9856843927d0674fd")))
-   (is (= (encrypt-aes-cbc data (HEX "984ca75f4ee8d706f46c2d98c0bf4a45f5b00d791c2dfeb191b5ed8e420fd627") iv) (HEX "4307456a9e67813b452e15fa8fffe398")))
-   (is (= (encrypt-aes-cbc data (HEX "b43d08a447ac8609baadae4ff12918b9f68fc1653f1269222f123981ded7a92f") iv) (HEX "4663446607354989477a5c6f0f007ef4")))
-   (is (= (encrypt-aes-cbc data (HEX "1d85a181b54cde51f0e098095b2962fdc93b51fe9b88602b3f54130bf76a5bd9") iv) (HEX "531c2c38344578b84d50b3c917bbb6e1")))
-   (is (= (encrypt-aes-cbc data (HEX "dc0eba1f2232a7879ded34ed8428eeb8769b056bbaf8ad77cb65c3541430b4cf") iv) (HEX "fc6aec906323480005c58e7e1ab004ad")))
-   (is (= (encrypt-aes-cbc data (HEX "f8be9ba615c5a952cabbca24f68f8593039624d524c816acda2c9183bd917cb9") iv) (HEX "a3944b95ca0b52043584ef02151926a8")))
-   (is (= (encrypt-aes-cbc data (HEX "797f8b3d176dac5b7e34a2d539c4ef367a16f8635f6264737591c5c07bf57a3e") iv) (HEX "a74289fe73a4c123ca189ea1e1b49ad5")))
-   (is (= (encrypt-aes-cbc data (HEX "6838d40caf927749c13f0329d331f448e202c73ef52c5f73a37ca635d4c47707") iv) (HEX "b91d4ea4488644b56cf0812fa7fcf5fc")))
-   (is (= (encrypt-aes-cbc data (HEX "ccd1bc3c659cd3c59bc437484e3c5c724441da8d6e90ce556cd57d0752663bbc") iv) (HEX "304f81ab61a80c2e743b94d5002a126b")))
-   (is (= (encrypt-aes-cbc data (HEX "13428b5e4c005e0636dd338405d173ab135dec2a25c22c5df0722d69dcc43887") iv) (HEX "649a71545378c783e368c9ade7114f6c")))
-   (is (= (encrypt-aes-cbc data (HEX "07eb03a08d291d1b07408bf3512ab40c91097ac77461aad4bb859647f74f00ee") iv) (HEX "47cb030da2ab051dfc6c4bf6910d12bb")))
-   (is (= (encrypt-aes-cbc data (HEX "90143ae20cd78c5d8ebdd6cb9dc1762427a96c78c639bccc41a61424564eafe1") iv) (HEX "798c7c005dee432b2c8ea5dfa381ecc3")))
-   (is (= (encrypt-aes-cbc data (HEX "b7a5794d52737475d53d5a377200849be0260a67a2b22ced8bbef12882270d07") iv) (HEX "637c31dc2591a07636f646b72daabbe7")))
-   (is (= (encrypt-aes-cbc data (HEX "fca02f3d5011cfc5c1e23165d413a049d4526a991827424d896fe3435e0bf68e") iv) (HEX "179a49c712154bbffbe6e7a84a18e220"))) )
+   (test-aes-cbc data (HEX "c47b0294dbbbee0fec4757f22ffeee3587ca4730c3d33b691df38bab076bc558") iv (HEX "46f2fb342d6f0ab477476fc501242c5f"))
+   (test-aes-cbc data (HEX "28d46cffa158533194214a91e712fc2b45b518076675affd910edeca5f41ac64") iv (HEX "4bf3b0a69aeb6657794f2901b1440ad4"))
+   (test-aes-cbc data (HEX "c1cc358b449909a19436cfbb3f852ef8bcb5ed12ac7058325f56e6099aab1a1c") iv (HEX "352065272169abf9856843927d0674fd"))
+   (test-aes-cbc data (HEX "984ca75f4ee8d706f46c2d98c0bf4a45f5b00d791c2dfeb191b5ed8e420fd627") iv (HEX "4307456a9e67813b452e15fa8fffe398"))
+   (test-aes-cbc data (HEX "b43d08a447ac8609baadae4ff12918b9f68fc1653f1269222f123981ded7a92f") iv (HEX "4663446607354989477a5c6f0f007ef4"))
+   (test-aes-cbc data (HEX "1d85a181b54cde51f0e098095b2962fdc93b51fe9b88602b3f54130bf76a5bd9") iv (HEX "531c2c38344578b84d50b3c917bbb6e1"))
+   (test-aes-cbc data (HEX "dc0eba1f2232a7879ded34ed8428eeb8769b056bbaf8ad77cb65c3541430b4cf") iv (HEX "fc6aec906323480005c58e7e1ab004ad"))
+   (test-aes-cbc data (HEX "f8be9ba615c5a952cabbca24f68f8593039624d524c816acda2c9183bd917cb9") iv (HEX "a3944b95ca0b52043584ef02151926a8"))
+   (test-aes-cbc data (HEX "797f8b3d176dac5b7e34a2d539c4ef367a16f8635f6264737591c5c07bf57a3e") iv (HEX "a74289fe73a4c123ca189ea1e1b49ad5"))
+   (test-aes-cbc data (HEX "6838d40caf927749c13f0329d331f448e202c73ef52c5f73a37ca635d4c47707") iv (HEX "b91d4ea4488644b56cf0812fa7fcf5fc"))
+   (test-aes-cbc data (HEX "ccd1bc3c659cd3c59bc437484e3c5c724441da8d6e90ce556cd57d0752663bbc") iv (HEX "304f81ab61a80c2e743b94d5002a126b"))
+   (test-aes-cbc data (HEX "13428b5e4c005e0636dd338405d173ab135dec2a25c22c5df0722d69dcc43887") iv (HEX "649a71545378c783e368c9ade7114f6c"))
+   (test-aes-cbc data (HEX "07eb03a08d291d1b07408bf3512ab40c91097ac77461aad4bb859647f74f00ee") iv (HEX "47cb030da2ab051dfc6c4bf6910d12bb"))
+   (test-aes-cbc data (HEX "90143ae20cd78c5d8ebdd6cb9dc1762427a96c78c639bccc41a61424564eafe1") iv (HEX "798c7c005dee432b2c8ea5dfa381ecc3"))
+   (test-aes-cbc data (HEX "b7a5794d52737475d53d5a377200849be0260a67a2b22ced8bbef12882270d07") iv (HEX "637c31dc2591a07636f646b72daabbe7"))
+   (test-aes-cbc data (HEX "fca02f3d5011cfc5c1e23165d413a049d4526a991827424d896fe3435e0bf68e") iv (HEX "179a49c712154bbffbe6e7a84a18e220")) )
  ; 128-bit, 2 blocks
  (let [ 
         key-128    (HEX "c286696d887c9aa0611bbb3e2025a45a")
@@ -105,7 +118,7 @@
         iv         (HEX "562e17996d093d28ddb3ba695a2e6f58")
         result     (HEX "d296cd94c2cccf8a3a863028b5e1dc0a7586602d253cfff91b8266bea6d61ab1")
       ]
-   (is (= (encrypt-aes-cbc data key-128 iv) result)))
+   (test-aes-cbc data key-128 iv result))
  ; 128-bit, 3 blocks
  (let [ 
         key-128    (HEX "6c3ea0477630ce21a2ce334aa746c2cd")
@@ -113,7 +126,7 @@
         iv         (HEX "c782dc4c098c66cbd9cd27d825682c81")
         result     (HEX "d0a02b3836451753d493665d33f0e8862dea54cdb293abc7506939276772f8d5021c19216bad525c8579695d83ba2684")
       ]
-   (is (= (encrypt-aes-cbc data key-128 iv) result)))
+   (test-aes-cbc data key-128 iv result))
  ; 128-bit, 4 blocks
  (let [ 
         key-128    (HEX "56e47a38c5598974bc46903dba290349")
@@ -121,4 +134,4 @@
         iv         (HEX "8ce82eefbea0da3c44699ed7db51b7d9")
         result     (HEX "c30e32ffedc0774e6aff6af0869f71aa0f3af07a9a31a9c684db207eb0ef8e4e35907aa632c3ffdf868bb7b29d3d46ad83ce9f9a102ee99d49a53e87f4c3da55")
       ]
-   (is (= (encrypt-aes-cbc data key-128 iv) result))) )
+   (test-aes-cbc data key-128 iv result)) )
