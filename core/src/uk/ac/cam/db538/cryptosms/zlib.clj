47eee6b504bf004700f79377f4c0f2ffb6194361
David Brázdil <david.brazdil@gmail.com>
2011-10-16 02:09:07 +0100

    Throws error for too short inputs to Zlib/inflate

--- core/src/uk/ac/cam/db538/cryptosms/zlib.clj
+++ core/src/uk/ac/cam/db538/cryptosms/zlib.clj
@@ -3,7 +3,7 @@
         [uk.ac.cam.db538.cryptosms.utils :only (HEX ASCII) ])
   (:require [uk.ac.cam.db538.cryptosms.utils :as utils]
             [uk.ac.cam.db538.cryptosms.low-level.byte-arrays :as byte-arrays] )
-  (:import (java.util.zip Inflater Deflater)
+  (:import (java.util.zip Inflater Deflater DataFormatException)
            (java.io ByteArrayOutputStream) )) 
 
 (defn deflate
@@ -25,22 +25,28 @@
 (defn inflate
   "Decompresses given data (byte vector) with DEFLATE"
   [ ^bytes data ]
-  (let [ buffer      (byte-arrays/create 1024)
-         inflater    (new Inflater)
-         bos         (new ByteArrayOutputStream (count data))
-         data-bytes  (byte-arrays/output (persistent! (reduce conj! (transient [ 120 156 ]) data)) ) ] ; prepends ZLIB header
-    (. inflater setInput data-bytes)
-    (loop [ ]
-      (if (. inflater finished)
-        (byte-arrays/input (. bos toByteArray))
-        (let [ decompressed (. inflater inflate buffer) ]
-          (. bos write buffer 0 decompressed)
-          (recur))))))
+  (if (< (count data) 6)
+    (throw (new IllegalArgumentException)) ; too short
+    (let [ buffer      (byte-arrays/create 1024)
+           inflater    (new Inflater)
+           bos         (new ByteArrayOutputStream (count data))
+           data-bytes  (byte-arrays/output (persistent! (reduce conj! (transient [ 120 156 ]) data)) ) ] ; prepends ZLIB header
+      (. inflater setInput data-bytes)
+      (loop [ ]
+        (if (. inflater finished)
+          (byte-arrays/input (. bos toByteArray))
+          (let [ decompressed (. inflater inflate buffer) ]
+            (. bos write buffer 0 decompressed)
+            (recur)))))))
 
 (with-test
   (defn- test-deflate-inflate [ data ]
     (is (= (inflate (deflate data)) data)))
   (test-deflate-inflate (ASCII "Hello World!"))
   (test-deflate-inflate (ASCII "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper tempor nibh ut fringilla. Etiam accumsan ultrices varius. Proin fermentum ligula tempor ipsum tristique eget imperdiet tellus tempus. Morbi quis mauris lorem. Sed in dui ipsum, vel viverra tortor. Suspendisse dapibus nibh ut ante mattis sit amet scelerisque lorem sollicitudin. Maecenas at ipsum sit amet nibh dignissim bibendum. Sed lobortis pharetra sem ut bibendum."))
-  (test-deflate-inflate (HEX "00112233445566778899aabbccddeeff")))
+  (test-deflate-inflate (HEX "00112233445566778899aabbccddeeff"))
+  (is (thrown? IllegalArgumentException (inflate [ ])))
+  (is (thrown? IllegalArgumentException (inflate [ 1 ])))
+  (is (thrown? IllegalArgumentException (inflate [ 1 2 3 4 5 ])))
+  (is (thrown? DataFormatException (inflate [ 1 2 3 4 5 6 ]))) )
 
