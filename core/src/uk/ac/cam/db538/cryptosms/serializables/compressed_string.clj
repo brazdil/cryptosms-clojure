733d40914f14665f7c6e5ecef391025b8fdfd2ac
David Brázdil <david.brazdil@gmail.com>
2011-10-23 23:08:25 +0100

    Compressed string serializable

---
+++ core/src/uk/ac/cam/db538/cryptosms/serializables/compressed_string.clj
@@ -0,0 +1,59 @@
+(ns uk.ac.cam.db538.cryptosms.serializables.compressed-string
+  (:use [clojure.test :only (with-test, is) ]
+        [uk.ac.cam.db538.cryptosms.utils :only (HEX) ])
+  (:require [uk.ac.cam.db538.cryptosms.utils :as utils]
+            [uk.ac.cam.db538.cryptosms.compressed-string :as compressed-string]
+            [uk.ac.cam.db538.cryptosms.serializables.common :as common] ) )
+
+(with-test
+  (defn compressed-string
+    "Returns a serializable type that stores String using the best possible
+     combination of encoding and DEFLATE compression, such that the size
+     is least possible and no data is lost."
+    [key-name]
+    (uk.ac.cam.db538.cryptosms.serializables.common.Serializable.
+      ; export
+      (fn [data]
+        (compressed-string/compressed-string (data key-name)))
+      ; import
+      (fn [^bytes xs args] 
+        (if (< (count xs) 1)
+          (throw (new IllegalArgumentException))
+          {key-name (compressed-string/compressed-string xs)} ))
+      ; length
+      (fn [data] (count (compressed-string/compressed-string (data key-name)))) ))
+  ; tests just check that everything is integrated together correctly... string compression methods are tested separately
+  (let [ data                   {:string "Hello world! I'm CryptoSMS"}
+         serializable           (compressed-string :string)
+         exported               ((:export serializable) data)
+         imported               ((:import serializable) exported {}) ]
+    (is (= data imported))
+    (is (= (count exported) 24))
+    (is (= ((:length serializable) data) 24 ))))
+
+(with-test
+  (defn compressed-string-aligned
+    "Returns a serializable type that stores String using the best possible
+     combination of encoding and DEFLATE compression, such that the size
+     is least possible and no data is lost. Data is aligned to 16 bytes
+     (useful for encryption)."
+    [key-name]
+    (uk.ac.cam.db538.cryptosms.serializables.common.Serializable.
+      ; export
+      (fn [data]
+        (compressed-string/compressed-string-aligned (data key-name)))
+      ; import
+      (fn [^bytes xs args] 
+        (if (< (count xs) 16)
+          (throw (new IllegalArgumentException))
+          {key-name (compressed-string/compressed-string-aligned xs)} ))
+      ; length
+      (fn [data] (count (compressed-string/compressed-string-aligned (data key-name)))) ))
+  ; tests just check that everything is integrated together correctly... string compression methods are tested separately
+  (let [ data                   {:string "Hello world! I'm CryptoSMS"}
+         serializable           (compressed-string-aligned :string)
+         exported               ((:export serializable) data)
+         imported               ((:import serializable) exported {}) ]
+    (is (= data imported))
+    (is (= (count exported) 32))
+    (is (= ((:length serializable) data) 32 ))))
