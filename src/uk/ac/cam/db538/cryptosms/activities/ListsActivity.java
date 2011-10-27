7cf759aa949a92a6bb71fc66a1766235c31872f0
David Brázdil <david.brazdil@gmail.com>
2011-10-28 00:31:05 +0100

    Buttons in ActionBar, light theme

--- src/uk/ac/cam/db538/cryptosms/activities/ListsActivity.java
+++ src/uk/ac/cam/db538/cryptosms/activities/ListsActivity.java
@@ -2,7 +2,10 @@ package uk.ac.cam.db538.cryptosms.activities;
 
 import uk.ac.cam.db538.cryptosms.R;
 import greendroid.app.GDActivity;
+import greendroid.widget.ActionBarItem;
+import greendroid.widget.ActionBarItem.Type;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ListsActivity extends GDActivity {
 
@@ -10,6 +13,28 @@ public class ListsActivity extends GDActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        addActionBarItem(Type.Talk, R.id.action_bar_threads);
+        addActionBarItem(Type.AllFriends, R.id.action_bar_contacts);
+        addActionBarItem(Type.Star, R.id.action_bar_notifications);
+        
         setActionBarContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onHandleActionBarItemClick(ActionBarItem item, int position) {
+        switch (item.getItemId()) {
+            case R.id.action_bar_threads:
+                Toast.makeText(this, "Threads", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_bar_contacts:
+                Toast.makeText(this, "Contacts", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_bar_notifications:
+                Toast.makeText(this, "Notifications", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return super.onHandleActionBarItemClick(item, position);
+        }
+        return true;
     }
 }
